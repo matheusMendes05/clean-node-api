1ee65812e24fe0cbd22cdec7ae452d3e008eb7fc
matheus_mendes5 <Matheus.Lima@3pbrasil.com.br>
2023-08-16 20:19:15 -0300

    test: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,26 +1,30 @@
-import { AccountModel } from '../../../domain/models/account';
 import {
+  AccountModel,
   AddAccount,
   AddAccountModel,
-} from '../../../domain/usecases/add-account';
-import { Encrypter } from '../../protocols/encrypter';
+  AddAccountRepository,
+  Encrypter,
+} from './db-add-account-protocols';
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter;
+  private readonly addAccountRepository: AddAccountRepository;
 
-  constructor(encrypter: Encrypter) {
+  constructor(
+    encrypter: Encrypter,
+    addAccountRepository: AddAccountRepository
+  ) {
     this.encrypter = encrypter;
+    this.addAccountRepository = addAccountRepository;
   }
 
-  async add(account: AddAccountModel): Promise<AccountModel> {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
     // TS nao deixa retornar um valor null pois o metodo deve retornar um Obj do tipo AccountModel
-    const data = {
-      id: 'any_id',
-      name: account.name,
-      email: account.email,
-      password: await this.encrypter.encrypt(account.password),
-    };
+    const hashedPassword = await this.encrypter.encrypt(accountData.password);
+    const account = await this.addAccountRepository.add(
+      Object.assign({}, accountData, { password: hashedPassword })
+    );
 
-    return new Promise((resolve) => resolve(data));
+    return new Promise((resolve) => resolve(account));
   }
 }
