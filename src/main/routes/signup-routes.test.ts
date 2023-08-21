8f9b2354c38cb1d6361617a1d8a9f2e86d1b3a2c
matheus_mendes5 <Matheus.Lima@3pbrasil.com.br>
2023-08-21 20:06:14 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,23 @@
 import request from 'supertest';
 import app from '../config/app';
+import 'dotenv/config';
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 
+const uri = process.env.MONGO_URL;
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    if (uri !== undefined) await MongoHelper.connect(uri);
+  }, 5000);
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    accountCollection.deleteMany({});
+  });
+
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
