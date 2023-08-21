1b8b5ac8e3bcb61a65cae8d704cea2e4fb7d5c53
matheus_mendes5 <Matheus.Lima@3pbrasil.com.br>
2023-08-21 16:56:50 -0300

    feat: ensure requests parse body as json

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type { Config } from 'jest';
-
-const config: Config = {
+const config = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -196,4 +194,4 @@ const config: Config = {
   // watchman: true,
 };
 
-export default config;
+module.exports = config;
